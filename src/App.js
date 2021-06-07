9cbc098e9aaae5a2c6efdfce37c3e767e9320be0
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-06-07 18:33:03 -0300

    Desafio Realizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import About from './components/About';
+import Users from './components/Users';
+import StrictAccess from './components/StrictAccess';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+        <BrowserRouter>
+          <Switch>
+            <Route path='/users/:id' render={(props) => <Users {...props} greetingsMessage="Good Morning"/> } />
+            <Route path='/StrictAccess' render={(props) => <StrictAccess {...props} user={{username: 'joao', password: '1234'}} /> } />
+            <Route path='/about'><About /></Route>
+            <Route path='/'><Home /></Route>
+          </Switch>
+        </BrowserRouter>
     );
   }
 }
